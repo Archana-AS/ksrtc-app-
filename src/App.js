2214739331b74259b3773a1852f381c28b11b0e3
Archana-AS <archanaas2321@gmail.com>
2024-04-14 22:00:52 +0530

    project Completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter , Routes , Route} from 'react-router-dom';
+import Buses from './components/Buses';
+import Search from './components/Search';
+import AddBus from './components/AddBus';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+     <BrowserRouter>
+     <Routes >
+   <Route path ='/' element={<Buses/>}/>
+   <Route path ='/Search' element={<Search />} />
+   <Route path ='/Add' element={<AddBus />} />
+      </Routes> 
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/Buses.jsx
@@ -0,0 +1,119 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+const Buses = () => {
+  return (
+    <div>
+        <Navbar/>
+       <div className="container">
+                <div className="row">
+                    <div className="col col-12 col-sm-12 col-md-12 col-xl-12 col-xxl-12">
+
+                        <div className="row g-3">
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                                <div class="card" >
+                                    <img src="https://english.mathrubhumi.com/image/contentid/policy:1.7662534:1656931322/New%20Project.jpg?$p=8ce1a8e&f=16x10&w=852&q=0.8" class="card-img-top" alt="..."/>
+                                        <div class="card-body">
+                                            <p class="card-text">Kannur</p>
+                                        </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                                <div class="card" >
+                                    <img src="https://s1.rdbuz.com/bo-images/IND/WM/27000/1934/FR/ML/webp/VztESv.webp" class="card-img-top" alt="..."/>
+                                        <div class="card-body">
+                                            <p class="card-text">Vyanad.</p>
+                                        </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                                <div class="card" >
+                                    <img src="https://images.livemint.com/img/2023/02/22/600x338/bengaluru_1677042673712_1677042680029_1677042680029.jpg" class="card-img-top" alt="..."/>
+                                        <div class="card-body">
+                                            <p class="card-text">Idukki.</p>
+                                        </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                                <div class="card" >
+                                    <img src="https://www.volvobuses.com/content/dam/volvo-buses/markets/global/classic/news/2020/1860x1050-Volvo-Bus-KSRTC-2020.jpg" class="card-img-top" alt="..."/>
+                                        <div class="card-body">
+                                            <p class="card-text">Kasargod</p>
+                                        </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                                <div class="card" >
+                                    <img src="https://www.india.com/wp-content/uploads/2023/08/KSRTC.jpg" class="card-img-top" alt="..."/>
+                                        <div class="card-body">
+                                            <p class="card-text">Pathanamthitta</p>
+                                        </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                                <div class="card" >
+                                    <img src="https://www.team-bhp.com/sites/default/files/styles/check_high_res/public/kerala-srtc.jpg" class="card-img-top" alt="..."/>
+                                        <div class="card-body">
+                                            <p class="card-text">Thrivananthapuram </p>
+                                        </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                                <div class="card" >
+                                    <img src="https://media.assettype.com/thenewsminute%2Fimport%2Fsites%2Fdefault%2Ffiles%2Fksrtc_new_0.jpg?w=480&auto=format%2Ccompress&fit=max" class="card-img-top" alt="..."/>
+                                        <div class="card-body">
+                                            <p class="card-text">Alapuzha route.</p>
+                                        </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                                <div class="card" >
+                                    <img src="https://fottam.com/wp-content/uploads/2020/05/KSRTC-Superfast-RPK489-brand-new-first-run-kottayam-.jpg" class="card-img-top" alt="..."/>
+                                        <div class="card-body">
+                                            <p class="card-text">Palakad  route</p>
+                                        </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                                <div class="card" >
+                                    <img src="https://s1.rdbuz.com/bo-images/IND/WM/19393/1118/FR/ML/webp/0M8G8n.webp" class="card-img-top" alt="..."/>
+                                        <div class="card-body">
+                                            <p class="card-text">Trissur Route</p>
+                                        </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                                <div class="card" >
+                                    <img src="https://i1.wp.com/fottam.com/wp-content/uploads/2017/10/KSRTC-Eicher-SuperFast-RSC990-Kottayam.jpg?fit=1160%2C652&ssl=1" class="card-img-top" alt="..."/>
+                                        <div class="card-body">
+                                            <p class="card-text">cottayam route</p>
+                                        </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                                <div class="card" >
+                                    <img src="https://images.livemint.com/img/2023/02/22/600x338/bengaluru_1677042673712_1677042680029_1677042680029.jpg" class="card-img-top" alt="..."/>
+                                        <div class="card-body">
+                                            <p class="card-text">Aluva Route</p>
+                                        </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                                <div class="card" >
+                                    <img src="https://1.bp.blogspot.com/-m4wd3EL_M_Y/VqYURpDL4vI/AAAAAAAAA0M/e-LwUj2yL6c/s1600/JN%2B79%2BKL-15-9929.jpg" class="card-img-top" alt="..."/>
+                                        <div class="card-body">
+                                            <p class="card-text">Ernakulam Route</p>
+                                        </div>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+    </div>
+    </div>
+
+  )
+}
+
+export default Buses
